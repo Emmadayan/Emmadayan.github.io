735af21fcc501624161e4d199fbd32912dd23917
Emmadayan <46944765+Emmadayan@users.noreply.github.com>
2019-09-26 18:47:18 -0400

    Add files via upload

--- final/assets/script.js
+++ final/assets/script.js
@@ -5,3 +5,12 @@ console.log (
 // let myDevice = window.navigator.userAgent;
 // document.getElementById("device").innrHTML = myDevice;
 
+$( "path" ).click(function() {
+    alert( "clicked" );
+  });
+
+$(document).ready(function() {
+    $("a").click(function() {
+        alert("Hello world!");
+    });
+});
